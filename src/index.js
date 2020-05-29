b701594727b98d1f50f9dbfd23371d5cee2d5eac
angela <angela@aviak.ca>
2020-05-28 20:45:35 -0600

    split reducers and combine them with combineReducers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './redux/reducers'
+import rootReducer from './redux/reducers'
 import App from './components/App'
 
-const store = createStore(todoApp)
+const store = createStore(rootReducer)
 
 render(
   <Provider store={store}>
